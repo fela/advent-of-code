26e894c9ddc0f560c728d431160ad957ca95fc27
Fela Calana <fela@users.noreply.github.com>
2023-12-02 09:44:41 +0100

    Another minor cosmetic change

--- 2023/01_minimalist.py
+++ 2023/01_minimalist.py
@@ -1,17 +1,16 @@
 # A more minimalist solution
 
 data = open('02.txt').readlines()
-
+digit_names = 'one two three four five six seven eight nine'.split()
 
 def solve(part):
     tot = 0
     for line in data:
         if part == 'second':
-            for i, digit in enumerate('one two three four five six seven eight nine'.split()):
-                line = line.replace(digit, f'{digit}{i+1}{digit}')
+            for i, name in enumerate(digit_names):
+                line = line.replace(name, f'{name}{i+1}{name}')
         digits = [c for c in line if c.isdigit()]
         tot += int(digits[0] + digits[-1])
     return tot
-
 
 print(solve('first'), solve('second'))
