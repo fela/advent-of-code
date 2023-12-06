edd6ff14ac1cdc976d2a2902454c0c542b4e4220
Fela Calana <fela@users.noreply.github.com>
2023-12-06 11:30:48 +0100

    Day 6

---
+++ 2023/06.py
@@ -0,0 +1,18 @@
+import re
+from math import floor, ceil
+
+part1, part2 = [], []
+for line in open('06.txt').readlines():
+    nums = re.findall(r'(\d+)', line)
+    part1.append([int(s) for s in nums])
+    part2.append([int(''.join(nums))])
+
+def solve(data):
+    prod = 1
+    for t, d in zip(*data):
+        first = floor((t - (t*t-4*d)**0.5)/2 + 1)
+        last = ceil((t + (t*t-4*d)**0.5)/2 - 1)
+        prod *= (last-first+1)
+    return prod
+
+print(solve(part1), solve(part2))
