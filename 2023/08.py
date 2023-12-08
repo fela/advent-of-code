c57c3d7fc496009bc304ac6656a9d88a5f1e4692
Fela Calana <fela@users.noreply.github.com>
2023-12-08 07:41:03 +0100

    Slight cleanup

--- 2023/08.py
+++ 2023/08.py
@@ -6,23 +6,20 @@ data = open('d/08').read().strip()
 dirs, rest = data.split('\n\n')
 graph = {}
 
+dirs = [0 if d == 'L' else 1 for d in dirs]
 for a, b, c in re.findall(r'(\w\w\w) = \((\w\w\w), (\w\w\w)\)', rest):
     graph[a] = b, c
 
-dirs = [0 if d == 'L' else 1 for d in dirs]
-
-
-current = [v for v in graph if v.endswith('A')]
-
-vals = []
-for i, el in enumerate(current):
-    val = 0
+starts = [v for v in graph if v.endswith('A')]
+periods = []
+for i, el in enumerate(starts):
+    period = 0
     while True:
         for d in dirs:
             el = graph[el][d]
-            val += 1
-        if el[2] == 'Z':
-            vals.append(val)
+            period += 1
+        if el.endswith('Z'):
+            periods.append(period)
             break
 
-print(vals[0], np.lcm.reduce(vals))
+print(periods[0], np.lcm.reduce(periods))
