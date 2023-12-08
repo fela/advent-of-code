b4997708130af14d36b7b445739b502c5f2c14ca
Fela Calana <fela@users.noreply.github.com>
2023-12-08 07:38:03 +0100

    Remove unused code

--- 2023/08.py
+++ 2023/08.py
@@ -4,25 +4,22 @@ import numpy as np
 data = open('d/08').read().strip()
 
 dirs, rest = data.split('\n\n')
-map = {}
+graph = {}
 
 for a, b, c in re.findall(r'(\w\w\w) = \((\w\w\w), (\w\w\w)\)', rest):
-    map[a] = b, c
+    graph[a] = b, c
 
 dirs = [0 if d == 'L' else 1 for d in dirs]
 
 
-current = [v for v in map if v.endswith('A')]
-s_to_i = {s: i for i, s in enumerate(map.keys())}
-
-sets = [set() for _ in current]
+current = [v for v in graph if v.endswith('A')]
 
 vals = []
 for i, el in enumerate(current):
     val = 0
     while True:
         for d in dirs:
-            el = map[el][d]
+            el = graph[el][d]
             val += 1
         if el[2] == 'Z':
             vals.append(val)
