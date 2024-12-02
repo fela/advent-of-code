3cefba4c657ddaafc86bf13271f07e64b15c31a8
Fela Calana <fela@users.noreply.github.com>
2024-12-02 14:04:50 +0200

    Can't help myself

--- 2024/01.py
+++ 2024/01.py
@@ -2,10 +2,9 @@ from collections import Counter
 
 data = open('d/01').read().splitlines()
 l1, l2 = zip(*(map(int, el.split()) for el in data))
-
-part1 = sum(abs(x-y) for x, y in zip(sorted(l1), sorted(l2)))
-
 counts = Counter(l2)
-part2 = sum(counts[x]*x for x in l1)
 
-print(part1, part2)
+print(
+    sum(abs(x-y) for x, y in zip(sorted(l1), sorted(l2))),
+    sum(counts[x]*x for x in l1)
+)
