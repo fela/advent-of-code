061892af110bcaadcecc2a719cb1dc75d3e95765
Fela Calana <fela@users.noreply.github.com>
2024-12-02 13:58:12 +0200

    Reformatting

--- 2024/02.py
+++ 2024/02.py
@@ -3,7 +3,10 @@ data = [list(map(int, el.split())) for el in data]
 
 def check(vals):
     diffs = [x-y for x, y in zip(vals, vals[1:])]
-    return all(1 <= d <= 3 for d in diffs) or all(-3 <= d <= -1 for d in diffs)
+    return (
+        all(1 <= d <= 3 for d in diffs) or 
+        all(-3 <= d <= -1 for d in diffs)
+    )
 
 def check_part2(vals):
     return any(check(vals[:i]+vals[i+1:]) for i in range(len(vals)))
